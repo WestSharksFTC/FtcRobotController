67e7b679652ee974a79d755416ac85634f195ef1
West Sharks FTC <westsharksftc@gmail.com>
2025-09-07 04:41:56 -0300

    feat: implementação do outtake na arquitetura Orientada a Objetos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/RI30H/RI30HOuttake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/RI30H/RI30HOuttake.java
@@ -1,4 +1,24 @@
 package org.firstinspires.ftc.teamcode.Decode.RI30H;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class RI30HOuttake {
+    DcMotor motorSpinner;
+    public RI30HOuttake(HardwareMap hardwareMap){
+        // Mapeia o motor do spinner pelo seu nome de configuração no hardwareMap.
+        motorSpinner = hardwareMap.get(DcMotor.class, "Spinner"); // Motor Frontal Esquerdo
+
+        // Define o modo de operação do motore para rodar sem encoders (controle de potência).
+        motorSpinner.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Inverte a direção do motor do spinner para que o motor gire na direção correta para o movimento.
+        motorSpinner.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    public void spin(double joystick, double velo){
+         velo += joystick;
+        motorSpinner.setPower(velo);
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/RI30H/RI30HTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/RI30H/RI30HTeleOp.java
@@ -3,15 +3,24 @@ package org.firstinspires.ftc.teamcode.Decode.RI30H;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 public class RI30HTeleOp extends OpMode {
+    //instanciando as classes
     RI30HChassis chassis;
     RI30HIntake intake;
     RI30HOuttake outtake;
+
+    double velocity = 0;
+
+
     public void init(){
         chassis = new RI30HChassis(hardwareMap);
+        outtake = new RI30HOuttake(hardwareMap);
     }
+
     public void loop(){
-        telemetry.addLine("Angulo do robô: "+ chassis.imu.getRobotYawPitchRollAngles().getYaw());
+        telemetry.addLine("Angulo do robô: " + chassis.imu.getRobotYawPitchRollAngles().getYaw());
+        telemetry.addLine("Velocidade do spinner: " + velocity);
         chassis.andar(-gamepad1.left_stick_y, -gamepad1.right_stick_x, -gamepad1.right_stick_x, gamepad1.right_trigger, gamepad1.left_bumper);
+        outtake.spin(gamepad1.right_stick_y, velocity);
 
         telemetry.update();
     }
