80f60b1c6ed3c350f58c1a61442739243b11c349
West Sharks FTC <westsharksftc@gmail.com>
2025-10-29 09:53:59 -0300

    feat: conclusão do benchtest dos servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Mechanism/robotServoPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Mechanism/robotServoPractice.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Util.Mechanism;
 
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -12,6 +13,9 @@ public class robotServoPractice {
     public void init(HardwareMap hardwareMap){
         servoPos = hardwareMap.get(Servo.class, "servo");
         servoRot = hardwareMap.get(CRServo.class, "crservo");
+        servoPos.scaleRange(0.5, 1.0); // set range from midpoint to 180* (when is a 180* servo)
+        servoPos.setDirection(Servo.Direction.REVERSE);
+        servoRot.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     public void setServoPos(double angle){
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Mechanism/useRobotServoPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Mechanism/useRobotServoPractice.java
@@ -6,13 +6,34 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp
 public class useRobotServoPractice extends OpMode {
 
+    robotServoPractice servo = new robotServoPractice();
+    double leftTrigger, rightTrigger;
+
     @Override
     public void init() {
-        robotServoPractice servo = new robotServoPractice();
+        servo.init(hardwareMap);
+        leftTrigger = 0.0;
+        rightTrigger = 0.0;
     }
 
     @Override
     public void loop() {
-        servo.
+        leftTrigger = gamepad1.left_trigger;
+        rightTrigger = gamepad1.right_trigger;
+
+        servo.setServoPos(leftTrigger);
+        servo.setServoRot(rightTrigger);
+
+        if(gamepad1.a){
+            servo.setServoPos(-1.0);
+        }else{
+            servo.setServoPos(1.0);
+        }
+
+        if(gamepad1.b){
+            servo.setServoRot(1.0);
+        }else{
+            servo.setServoRot(0);
+        }
     }
 }
